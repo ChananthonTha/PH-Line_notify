5bc02d456df3b174f700c2e09a9e8db9499b1c64
ChananthonTha <koonzee14@gmail.com>
2023-01-11 18:32:31 +0700

    up-11-1-2566

---
+++ Line_Notify.ino
@@ -0,0 +1,37 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "xxxxxxxx";       //ชื่อ WiFi **2.4Ghz**
+const char* password = "xxxxxxxxx";  // รหัส WiFi
+const char* LINE_TOKEN = "xxxxxxx"   // Token Line Notify
+  //Line notify url:https://notify-bot.line.me/my/
+
+  void
+  setup() {
+  Serial.begin(115200);
+  Serial.println("Connecting...");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //Line Token
+  LINE.setToken(LINE_TOKEN);
+}
+
+void loop() {
+  sensor(); //Call sensor Function
+
+  // ตัวอย่างส่งข้อความ
+  LINE.notify("ค่า PH เกินกำหนด");
+  LINE.notify("PH:8.17");
+  delay(10000); // 1 Second = 1000
+}
+
+void sensor(){
+  //PH sensor
+}
